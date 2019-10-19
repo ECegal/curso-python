79d6e6181d19da1bd3a04bcae09eec24d9357256
ECegal <eder.cegal@gmail.com>
2019-10-19 16:41:46 -0300

    Exercicio de cadastro de usuario

---
+++ user_data_pag48.py
@@ -0,0 +1,15 @@
+print('***************************************')
+print('*********Cadastro de usuário***********')
+
+usuario = []
+
+nome = input('Insira o nome do usuario: ')
+usuario.append(nome)
+
+sobrenome = input ('Insira o sobrenome do usuário: ')
+usuario.append(sobrenome)
+
+idade = input ('Insira a idade do usuário: ')
+usuario.append(idade)
+
+print(usuario)
