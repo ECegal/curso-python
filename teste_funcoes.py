e11b3ded686a264d6010b492d8d0ba8d0619f77b
ECegal <eder.cegal@gmail.com>
2019-10-26 10:40:45 -0300

    Arquivo de teste de funçõe após a aula

--- teste_funcoes.py
+++ teste_funcoes.py
@@ -1,4 +1,19 @@
-def soma(*numeros):
-    print(sum(numeros))
+def soma(*numeros, mensagem = 'soma = '): # o caractere * define multiplos argumentos.
+    print(type(numeros))    
+    
+    resultado = { 'msg': mensagem,
+                  'soma' : sum(numeros)}
 
+    #print('{msg}{soma}'.format(msg=mensagem,soma = sum(numeros)))
+    print('{msg}{soma}'.format(**resultado))
+
+
+soma(1,2,3,4,mensagem = 'A soma é ') #Após o varargs, sempre deverá ser definido o próximo parametro
 soma(1,2,3,4)
+
+def diz_oi(**mensagem):
+    print ('{saudacao}, {pronome} {nome}'.format(**mensagem))
+
+mensagem = {'nome':'Eder','pronome':'Supremo senhor','saudacao' : 'Olá'}
+diz_oi(**mensagem)
+diz_oi(nome='Joao',pronome ='mestre',saudacao='Bom dia')
